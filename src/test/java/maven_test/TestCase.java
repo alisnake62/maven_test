b9fa447b24f609a21a29e049e249a5d846a52da5
Clement Sergent <clementsergent@yahoo.fr>
2022-01-04 16:37:24 +0100

    first commit

---
+++ src/test/java/maven_test/TestCase.java
@@ -0,0 +1,12 @@
+package maven_test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestCase {
+	@Test
+	public void testMonPremierTest() {
+		assertEquals(1,1);
+	}
+}
